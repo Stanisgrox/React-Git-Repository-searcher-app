84f2eb5e0aa06bc71023db295d2b1d50f002f2ce
Stanislav <orlandonetworks@gmail.com>
2024-08-08 02:57:55 +0300

    Footer bar added

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,13 @@
 import SearchBar from './components/search-bar/search-bar';
 import MainScreen from './components/main-screen/main-screen';
+import Footer from './components/footer/footer';
 
 function App() {
   return (
     <div className="App">
       <SearchBar />
       <MainScreen />
+      <Footer />
     </div>
   );
 }
---
+++ src/components/footer/footer.tsx
@@ -0,0 +1,9 @@
+import styles from './footer.module.scss';
+
+const Footer = () => {
+    return (
+        <div className={styles.footer} />
+    )
+}
+
+export default Footer;
